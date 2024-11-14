adb15104b34ee193e342c34fee77d40568fef069
Daniel-Runge <rungedaniel@hotmail.com>
2024-11-14 16:19:10 +0100

    Add remaining SUDD functions

--- src/cupaal/cudd_extensions.h
+++ src/cupaal/cudd_extensions.h
@@ -3,7 +3,15 @@
 #include <cuddObj.hh>
 
 namespace cupaal {
-    extern DdNode * Cudd_addExp(DdManager *dd, DdNode *f);
+    extern DdNode *Cudd_addExp(DdManager *dd, DdNode *f);
+
+    extern DdNode *Cudd_addLogMatrixMultiply(DdManager *dd, DdNode *A, DdNode *B, DdNode **z, int nz);
+
+    extern CUDD_VALUE_TYPE log_add(CUDD_VALUE_TYPE x, CUDD_VALUE_TYPE y);
+
+    extern DdNode *Cudd_addLogPlus(DdManager *dd, DdNode **f, DdNode **g);
+
+    extern DdNode *addLogMMRecur(DdManager *dd, DdNode *A, DdNode *B, int topP, int *vars);
 }
 
 #endif //CUDD_EXTENSIONS_H
