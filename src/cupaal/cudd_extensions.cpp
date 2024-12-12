07b13846a29af07af8985ebf41df420d2b9eba6b
Daniel-Runge <rungedaniel@hotmail.com>
2024-12-12 08:41:40 +0100

    correct calls for functions in cudd extensions. Add ADD_Read that reads adds from matrices

--- src/cupaal/cudd_extensions.cpp
+++ src/cupaal/cudd_extensions.cpp
@@ -18,7 +18,7 @@ DdNode *cupaal::Cudd_addExp(DdManager *dd, DdNode *f) {
     return nullptr;
 } /* end of Cudd_addExp */
 
-CUDD_VALUE_TYPE log_add(CUDD_VALUE_TYPE x, CUDD_VALUE_TYPE y) {
+CUDD_VALUE_TYPE cupaal::log_add(CUDD_VALUE_TYPE x, CUDD_VALUE_TYPE y) {
     if (y > x) {
         CUDD_VALUE_TYPE temp = x;
         x = y;
@@ -37,29 +37,29 @@ CUDD_VALUE_TYPE log_add(CUDD_VALUE_TYPE x, CUDD_VALUE_TYPE y) {
  *  @see Cudd_addApply
 */
 DdNode *
-Cudd_addLogPlus(
-  DdManager * dd,
-  DdNode ** f,
-  DdNode ** g)
-{
+cupaal::Cudd_addLogPlus(
+    DdManager *dd,
+    DdNode **f,
+    DdNode **g) {
     DdNode *res;
     DdNode *F, *G;
     CUDD_VALUE_TYPE value;
 
-    F = *f; G = *g;
-    if (cuddIsConstant(F) && cuddV(F) == -std::numeric_limits<double>::infinity()) return(F);
-    if (cuddIsConstant(G) && cuddV(G) == -std::numeric_limits<double>::infinity()) return(G);
+    F = *f;
+    G = *g;
+    if (cuddIsConstant(F) && cuddV(F) == -std::numeric_limits<double>::infinity()) return (F);
+    if (cuddIsConstant(G) && cuddV(G) == -std::numeric_limits<double>::infinity()) return (G);
     if (cuddIsConstant(F) && cuddIsConstant(G)) {
         value = log_add(cuddV(F), cuddV(G));
-        res = cuddUniqueConst(dd,value);
-        return(res);
+        res = cuddUniqueConst(dd, value);
+        return (res);
     }
-    if (F > G) { /* swap f and g */
+    if (F > G) {
+        /* swap f and g */
         *f = G;
         *g = F;
     }
-    return(nullptr);
-
+    return (nullptr);
 } /* end of Cudd_addPlus */
 
 DdNode *cupaal::Cudd_addLogMatrixMultiply(DdManager *dd, DdNode *A, DdNode *B, DdNode **z, int nz) {
@@ -91,7 +91,7 @@ DdNode *cupaal::Cudd_addLogMatrixMultiply(DdManager *dd, DdNode *A, DdNode *B, D
 
 
 DdNode *
-addLogMMRecur(
+cupaal::addLogMMRecur(
     DdManager *dd,
     DdNode *A,
     DdNode *B,
@@ -290,3 +290,207 @@ addLogMMRecur(
     cuddDeref(res);
     return (res);
 } /* end of addLogMMRecur */
+
+int cupaal::Sudd_addRead(
+    CUDD_VALUE_TYPE *array,
+    ssize_t array_n_rows,
+    ssize_t array_n_cols,
+    DdManager *dd,
+    DdNode **E /**< characteristic function of the graph */,
+    DdNode ***x /**< array of row variables */,
+    DdNode ***y /**< array of column variables */,
+    DdNode ***xn /**< array of complemented row variables */,
+    DdNode ***yn_ /**< array of complemented column variables */,
+    int *nx /**< number or row variables */,
+    int *ny /**< number or column variables */,
+    int *m /**< number of rows */,
+    int *n /**< number of columns */,
+    int bx /**< first index of row variables */,
+    int sx /**< step of row variables */,
+    int by /**< first index of column variables */,
+    int sy /**< step of column variables */
+) {
+    DdNode *one, *zero;
+    DdNode *w, *neW;
+    DdNode *minterm1;
+    int u, v, err, i, nv;
+    int lnx, lny;
+    CUDD_VALUE_TYPE val;
+    DdNode **lx, **ly, **lxn, **lyn;
+
+    one = DD_ONE(dd);
+    zero = DD_ZERO(dd);
+
+    u = array_n_rows;
+    v = array_n_cols;
+
+    *m = u;
+    /* Compute the number of x variables. */
+    lx = *x;
+    lxn = *xn;
+    u--; /* row and column numbers start from 0 */
+    for (lnx = 0; u > 0; lnx++) {
+        u >>= 1;
+    }
+    /* Here we rely on the fact that REALLOC of a null pointer is
+    ** translates to an ALLOC.
+    */
+    if (lnx > *nx) {
+        *x = lx = REALLOC(DdNode *, *x, lnx);
+        if (lx == NULL) {
+            dd->errorCode = CUDD_MEMORY_OUT;
+            return (0);
+        }
+        *xn = lxn = REALLOC(DdNode *, *xn, lnx);
+        if (lxn == NULL) {
+            dd->errorCode = CUDD_MEMORY_OUT;
+            return (0);
+        }
+    }
+
+    *n = v;
+    /* Compute the number of y variables. */
+    ly = *y;
+    lyn = *yn_;
+    v--; /* row and column numbers start from 0 */
+    for (lny = 0; v > 0; lny++) {
+        v >>= 1;
+    }
+    /* Here we rely on the fact that REALLOC of a null pointer is
+    ** translates to an ALLOC.
+    */
+    if (lny > *ny) {
+        *y = ly = REALLOC(DdNode *, *y, lny);
+        if (ly == NULL) {
+            dd->errorCode = CUDD_MEMORY_OUT;
+            return (0);
+        }
+        *yn_ = lyn = REALLOC(DdNode *, *yn_, lny);
+        if (lyn == NULL) {
+            dd->errorCode = CUDD_MEMORY_OUT;
+            return (0);
+        }
+    }
+
+    /* Create all new variables. */
+    for (i = *nx, nv = bx + (*nx) * sx; i < lnx; i++, nv += sx) {
+        do {
+            dd->reordered = 0;
+            lx[i] = cuddUniqueInter(dd, nv, one, zero);
+        } while (dd->reordered == 1);
+        if (lx[i] == NULL) {
+            if (dd->errorCode == CUDD_TIMEOUT_EXPIRED && dd->timeoutHandler) {
+                dd->timeoutHandler(dd, dd->tohArg);
+            }
+            return (0);
+        }
+        cuddRef(lx[i]);
+        do {
+            dd->reordered = 0;
+            lxn[i] = cuddUniqueInter(dd, nv, zero, one);
+        } while (dd->reordered == 1);
+        if (lxn[i] == NULL) {
+            if (dd->errorCode == CUDD_TIMEOUT_EXPIRED && dd->timeoutHandler) {
+                dd->timeoutHandler(dd, dd->tohArg);
+            }
+            return (0);
+        }
+        cuddRef(lxn[i]);
+    }
+    for (i = *ny, nv = by + (*ny) * sy; i < lny; i++, nv += sy) {
+        do {
+            dd->reordered = 0;
+            ly[i] = cuddUniqueInter(dd, nv, one, zero);
+        } while (dd->reordered == 1);
+        if (ly[i] == NULL) {
+            if (dd->errorCode == CUDD_TIMEOUT_EXPIRED && dd->timeoutHandler) {
+                dd->timeoutHandler(dd, dd->tohArg);
+            }
+            return (0);
+        }
+        cuddRef(ly[i]);
+        do {
+            dd->reordered = 0;
+            lyn[i] = cuddUniqueInter(dd, nv, zero, one);
+        } while (dd->reordered == 1);
+        if (lyn[i] == NULL) {
+            if (dd->errorCode == CUDD_TIMEOUT_EXPIRED && dd->timeoutHandler) {
+                dd->timeoutHandler(dd, dd->tohArg);
+            }
+            return (0);
+        }
+        cuddRef(lyn[i]);
+    }
+    *nx = lnx;
+    *ny = lny;
+
+    *E = dd->background; /* this call will never cause reordering */
+    cuddRef(*E);
+
+    for (int row = 0; row < array_n_rows; row++) {
+        for (int col = 0; col < array_n_cols; col++) {
+            val = array[row * array_n_cols + col];
+            if (val == 0)
+                continue;
+            u = row;
+            v = col;
+
+            minterm1 = one;
+            cuddRef(minterm1);
+
+            /* Build minterm1 corresponding to this arc */
+            for (i = lnx - 1; i >= 0; i--) {
+                if (u & 1) {
+                    w = Cudd_addApply(dd, Cudd_addTimes, minterm1, lx[i]);
+                } else {
+                    w = Cudd_addApply(dd, Cudd_addTimes, minterm1, lxn[i]);
+                }
+                if (w == NULL) {
+                    Cudd_RecursiveDeref(dd, minterm1);
+                    return (0);
+                }
+                cuddRef(w);
+                Cudd_RecursiveDeref(dd, minterm1);
+                minterm1 = w;
+                u >>= 1;
+            }
+            for (i = lny - 1; i >= 0; i--) {
+                if (v & 1) {
+                    w = Cudd_addApply(dd, Cudd_addTimes, minterm1, ly[i]);
+                } else {
+                    w = Cudd_addApply(dd, Cudd_addTimes, minterm1, lyn[i]);
+                }
+                if (w == NULL) {
+                    Cudd_RecursiveDeref(dd, minterm1);
+                    return (0);
+                }
+                cuddRef(w);
+                Cudd_RecursiveDeref(dd, minterm1);
+                minterm1 = w;
+                v >>= 1;
+            }
+            /* Create new constant node if necessary.
+            ** This call will never cause reordering.
+            */
+            neW = cuddUniqueConst(dd, val);
+            if (neW == NULL) {
+                Cudd_RecursiveDeref(dd, minterm1);
+                return (0);
+            }
+            cuddRef(neW);
+
+            w = Cudd_addIte(dd, minterm1, neW, *E);
+            if (w == NULL) {
+                Cudd_RecursiveDeref(dd, minterm1);
+                Cudd_RecursiveDeref(dd, neW);
+                return (0);
+            }
+            cuddRef(w);
+            Cudd_RecursiveDeref(dd, minterm1);
+            Cudd_RecursiveDeref(dd, neW);
+            Cudd_RecursiveDeref(dd, *E);
+            *E = w;
+        }
+    }
+    return (1);
+}
