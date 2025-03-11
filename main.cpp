9d6a56d88a601817d98fb311714741cae6c8ebbf
Daniel-Runge <rungedaniel@hotmail.com>
2025-03-11 21:11:40 +0100

    Fix all identified memory issues. Fix all identified Cudd reference issues. Add basic model parsing functionality. Remove a lot of unnecessary parts. Refactor basic functions.

--- main.cpp
+++ main.cpp
@@ -7,69 +7,28 @@
 #include "src/cupaal/cudd_extensions.h"
 #include "src/cupaal/helpers.h"
 
-void manual_algebraic_decision_diagram_variant() {
-    DdManager *dd_manager = Cudd_Init(0, 0,CUDD_UNIQUE_SLOTS,CUDD_CACHE_SLOTS, 0);
-    Cudd_SetEpsilon(dd_manager, 0);
-
-    double transition_function[] = {
-        0.6, 0.4,
-        0.3, 0.7
-    };
-
-    double labelling_function[] = {
-        0.3, 0.4,
-        0.4, 0.3,
-        0.3, 0.3
-    };
-
-    double initial_distribution[] = {0.7, 0.3};
-
-    cupaal::MarkovModel_ADD model;
-    model.manager = dd_manager;
-    model.states = {0, 1};
-    model.labels = {"r", "w", "b"};
-    model.observations.push_back({
-        "r", "w", "b", "b", "b", "b", "r", "w", "b", "b", "b", "b", "r", "w", "b", "b", "b", "b", "r", "w", "b", "b",
-        "b", "b"
-    });
-
-    model.initialize_helpers();
-    model.initialize_probabilities(transition_function, labelling_function, initial_distribution);
-    Cudd_PrintDebug(dd_manager, model.transition_add, 2, 4);
-
-    auto [iterations, microseconds, log_likelihood] = model.baum_welch_add(1000);
-    Cudd_PrintDebug(dd_manager, model.transition_add, 2, 4);
-
-    std::cout << "Iterations: " << iterations << std::endl;
-    std::cout << "Microseconds: " << microseconds << std::endl;
-    std::cout << "Log likelihood: " << log_likelihood << std::endl;
-
-    std::cout << "Remaining references (expecting 0): " << Cudd_CheckZeroRef(dd_manager) << std::endl;
-    Cudd_Quit(dd_manager);
-}
 
 int main(int argc, char *argv[]) {
     // storm::utility::setUp();
     // storm::settings::initializeAll("CuPAAL", "CuPAAL");
     const std::filesystem::path dir(getenv("HOME"));
-    const std::filesystem::path full_path = dir / "thesis" / "CuPAAL" ;
+    const std::filesystem::path full_path = dir / "thesis" / "CuPAAL";
     DdManager *dd_manager = Cudd_Init(0, 0,CUDD_UNIQUE_SLOTS,CUDD_CACHE_SLOTS, 0);
-    Cudd_SetEpsilon(dd_manager, 0);
+    // Cudd_SetEpsilon(dd_manager, 0);
 
-    cupaal::MarkovModel_HMM model;
+    cupaal::MarkovModel model;
     model.manager = dd_manager;
-
     model.initialize_from_file("../example-model.txt");
-    model.export_to_file("../example-model-output.txt");
     cupaal::write_dd_to_dot(model.manager, model.tau, (full_path / "tau.dot").c_str());
 
-    model.add_observation({"r",  "g", "b", "b" , "b", "g", "b", "r"});
-    model.baum_welch(1);
+    model.add_observation({{"r", "b", "g"}});
+    model.baum_welch(100);
+    model.export_to_file("../example-model-output.txt");
 
-    // Added for debugging purposes
     model.clean_up_cudd();
 
     std::cout << "Remaining references (expecting 0): " << Cudd_CheckZeroRef(dd_manager) << std::endl;
     Cudd_Quit(dd_manager);
+
     exit(EXIT_SUCCESS);
 }
--- src/cupaal/baum_welch.h
+++ src/cupaal/baum_welch.h
@@ -2,22 +2,12 @@
 #define BAUM_H
 #include <cuddObj.hh>
 #include <map>
-#include <set>
 
 #include "helpers.h"
 
 using state = int;
 using probability = CUDD_VALUE_TYPE;
 using label = std::string;
-
-inline std::ostream &operator<<(std::ostream &stream, DdNode *node) {
-    if (Cudd_IsConstant(node)) {
-        stream << Cudd_V(node);
-        return stream;
-    }
-    stream << node;
-    return stream;
-}
 
 // This file should contain all the functions related to the Baum-Welch algorithm: forward-backward, update-parameter-estimates, etc.
 namespace cupaal {
@@ -27,7 +17,7 @@ namespace cupaal {
         CUDD_VALUE_TYPE log_likelihood;
     };
 
-    class MarkovModel_HMM {
+    class MarkovModel {
     public:
         DdManager *manager;
         std::vector<std::string> states;
@@ -43,13 +33,21 @@ namespace cupaal {
         DdNode *pi;
         DdNode *row_cube;
 
-        void baum_welch(unsigned int max_iterations = 100);
+        [[nodiscard]] DdNode **calculate_alpha() const;
 
-        [[nodiscard]] DdNode **forward() const;
+        [[nodiscard]] DdNode **calculate_beta() const;
+
+        [[nodiscard]] DdNode **calculate_gamma(DdNode **alpha, DdNode **beta) const;
+
+        [[nodiscard]] DdNode **calculate_xi(DdNode **alpha, DdNode **beta) const;
+
+        void update_model_parameters(DdNode **gamma, DdNode **xi);
+
+        void baum_welch(unsigned int max_iterations = 100, double epsilon = 1e-6);
 
         void initialize_from_file(const std::string &filename);
 
-        void add_observation(std::vector<std::string> observation);
+        void add_observation(const std::vector<std::string> &observation);
 
         void export_to_file(const std::string &filename);
 
@@ -66,89 +64,13 @@ namespace cupaal {
         int n_row_vars = 0;
         int n_col_vars = 0;
         int n_vars = 0;
-        DdNode **row_vars = static_cast<DdNode **>(safe_malloc(sizeof(DdNode *), n_vars));
-        DdNode **col_vars = static_cast<DdNode **>(safe_malloc(sizeof(DdNode *), n_vars));
-        DdNode **comp_row_vars = static_cast<DdNode **>(safe_malloc(sizeof(DdNode *), n_vars));
-        DdNode **comp_col_vars = static_cast<DdNode **>(safe_malloc(sizeof(DdNode *), n_vars));
+        DdNode **row_vars = nullptr;
+        DdNode **col_vars = nullptr;
+        DdNode **comp_row_vars = nullptr;
+        DdNode **comp_col_vars = nullptr;
+
+        double calculate_log_likelihood(DdNode **alpha) const;
     };
-
-    class MarkovModel_Matrix {
-    public:
-        bool print_calculations = false;
-        std::set<state> states;
-        std::vector<label> labels;
-        std::map<label, state> label_index_map;
-        std::vector<std::vector<label> > observations;
-        probability *labelling_matrix; // labelling matrix
-        probability *transition_matrix; // P
-        probability *initial_distribution_vector; // pi
-        probability *omega_matrix; // omega
-
-        void initialize_model_parameters_randomly(int seed = 0);
-
-        void print_model_parameters() const;
-
-        void calculate_omega();
-    };
-
-    class MarkovModel_ADD {
-    public:
-        DdManager *manager;
-        bool print_calculations = false;
-        std::set<state> states;
-        std::vector<label> labels;
-        std::map<label, state> label_index_map;
-        std::vector<std::vector<label> > observations;
-        DdNode *transition_add; // P
-        DdNode *initial_distribution_add; // pi
-        DdNode **labelling_add; // labelling add
-        DdNode *row_cube;
-
-        void initialize_probabilities(probability *transition_function, probability *labelling_function,
-                                      probability *initial_distribution);
-
-        void initialize_helpers();
-
-        void initialize_model_parameters_randomly(std::mt19937 generator);
-
-        void print_model_parameters() const;
-
-        [[nodiscard]] DdNode **forward_add() const;
-
-        [[nodiscard]] DdNode **backward_add() const;
-
-        [[nodiscard]] DdNode **gamma_add(DdNode **alpha, DdNode **beta) const;
-
-        [[nodiscard]] DdNode **xi_add(DdNode **alpha, DdNode **beta) const;
-
-        void update_model_parameters_add(DdNode **gamma, DdNode **xi);
-
-        report baum_welch_add(unsigned int max_iterations = 100);
-
-    private:
-        int dump_n_rows = 0;
-        int dump_n_cols = 0;
-        int n_row_vars = 0;
-        int n_col_vars = 0;
-        int n_vars = 0;
-        DdNode **row_vars = static_cast<DdNode **>(safe_malloc(sizeof(DdNode *), n_vars));
-        DdNode **col_vars = static_cast<DdNode **>(safe_malloc(sizeof(DdNode *), n_vars));
-        DdNode **comp_row_vars = static_cast<DdNode **>(safe_malloc(sizeof(DdNode *), n_vars));
-        DdNode **comp_col_vars = static_cast<DdNode **>(safe_malloc(sizeof(DdNode *), n_vars));
-    };
-
-
-    extern std::vector<probability> forward_matrix(const MarkovModel_Matrix &model);
-
-    extern std::vector<probability> backward_matrix(const MarkovModel_Matrix &model);
-
-    extern std::vector<probability> gamma_matrix(const MarkovModel_Matrix &model, const std::vector<probability> &alpha,
-                                                 const std::vector<probability> &beta);
-
-    extern std::vector<probability> xi_matrix(const MarkovModel_Matrix &model, const std::vector<probability> &alpha,
-                                              const std::vector<probability> &beta);
-
-    extern MarkovModel_Matrix baum_welch_matrix(const MarkovModel_Matrix &model);
 }
 
 #endif //BAUM_H
