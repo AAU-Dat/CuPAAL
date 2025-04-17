fba6354dc3c217cf42e26022d9f0cb4600d6bd6c
Daniel-Runge <rungedaniel@hotmail.com>
2025-04-17 09:19:19 +0200

    experiments now download requisite files for learning

--- experiment.sh
+++ experiment.sh
@@ -3,16 +3,16 @@
 cupaal_postfix="_cupaal_run"
 models=(leader_sync crowds brp)
 
-#for model in "${models[@]}"
-#do
-#  for i in {0..9}
-#  do
-#    curl -s --create-dirs -o "experiments/$model/$model$cupaal_postfix$i.txt" -L "https://raw.githubusercontent.com/AAU-Dat/P10-Thesis/refs/heads/experiments-models/experiments/initial-models/$model$cupaal_postfix$i.txt" &
-#  done
-#  curl -s --create-dirs -o "experiments/$model/observations.txt" -L "https://raw.githubusercontent.com/AAU-Dat/P10-Thesis/refs/heads/experiments-models/experiments/observations/$model""_observations_cupaal.txt" &
-#done
-#echo "Downloading experiment files"
-#wait
+for model in "${models[@]}"
+do
+  for i in {0..9}
+  do
+    curl -s --create-dirs -o "experiments/$model/$model$cupaal_postfix$i.txt" -L "https://raw.githubusercontent.com/AAU-Dat/P10-Thesis/refs/heads/experiments-models/experiments/initial-models/$model$cupaal_postfix$i.txt" &
+  done
+  curl -s --create-dirs -o "experiments/$model/observations.txt" -L "https://raw.githubusercontent.com/AAU-Dat/P10-Thesis/refs/heads/experiments-models/experiments/observations/$model""_observations_cupaal.txt" &
+done
+echo "Downloading experiment files"
+wait
 
 for model in "${models[@]}"
 do
